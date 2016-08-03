801580efd22c055abf7796a81fca23315f3d22bc
kurjoz <v.kuriy@gmail.com>
2016-08-03 20:47:13 +0200

    Added singlepage script

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
-@import "imported_example.js"
+@import "../bower_components/fullpage.js/jquery.fullPage.js";
+@import "imported_example.js";
+
+//http://jsfiddle.net/reddy887/o4uywjrn/    -   extend with footer;
